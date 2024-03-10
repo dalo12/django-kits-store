0d22d46b90633289a7ba1ec6b3ba8976ebdb970e
David López <dalo_vidpez@hotmail.com.ar>
2024-03-10 17:36:06 -0300

    Added entities to django-admin

--- kitsStore/kits_management/admin.py
+++ kitsStore/kits_management/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Kit, Team
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Team)
+admin.site.register(Kit)
