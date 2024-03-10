e6739f7db01e3dedd45b8495ecc6af6f3a257dd0
David López <dalo_vidpez@hotmail.com.ar>
2024-03-10 17:21:15 -0300

    Created 'team' model

--- kitsStore/kits_management/models.py
+++ kitsStore/kits_management/models.py
@@ -5,6 +5,12 @@ class Category(models.Model):
     name = models.CharField(max_length = 16)
     description = models.TextField()
 
+class Team(models.Model):
+    name = models.charField(max_length = 32)
+    description = models.TextField()
+    country = models.charField(max_length = 32)
+    sport = models.charField(max_length = 16)
+
 class Kit(models.Model):
     picture = models.URLField()
     name = models.CharField(max_length = 64)
