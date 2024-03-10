3a826923758aabff2c4e17c37b42ee4d0682c501
David López <dalo_vidpez@hotmail.com.ar>
2024-03-10 17:18:05 -0300

    Created 'kits' model

--- kitsStore/kits_management/models.py
+++ kitsStore/kits_management/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Kit(models.Model):
+    picture = models.URLField()
+    name = models.CharField(max_length = 64)
+    year = models.DecimalField(max_digits = 4, decimal_places = 0)
+    price = models.DecimalField(decimal_places = 2)
+    size = models.CharField()
+    description = models.TextField()
