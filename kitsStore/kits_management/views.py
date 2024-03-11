3108319f7ff0dfeab1b69723063f09c93aff1bd8
David López <dalo_vidpez@hotmail.com.ar>
2024-03-11 18:26:48 -0300

    Added template views

--- kitsStore/kits_management/views.py
+++ kitsStore/kits_management/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hola!")
+    return render(request, "kits/index.html", {
+        "kits": Kit.objects.all()
+    })
