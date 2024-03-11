ca9a633a0135cad7252b0dae94cf8ea2ba8ed327
David López <dalo_vidpez@hotmail.com.ar>
2024-03-11 19:12:05 -0300

    Installed tailwind in the project

--- kitsStore/kitsStore/settings.py
+++ kitsStore/kitsStore/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'tailwind',
-    'django_browser_reload',
+    'theme',
     'kits_management',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+TAILWIND_APP_NAME = 'theme'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
