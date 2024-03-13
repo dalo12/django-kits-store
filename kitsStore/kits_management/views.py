d162ccb86efeca6d897eedea1d6e8037488038e4
David López <dalo_vidpez@hotmail.com.ar>
2024-03-13 17:39:20 -0300

    Added a view for add a nwe kit

--- kitsStore/kits_management/views.py
+++ kitsStore/kits_management/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category, Kit, Team
 
+TALLES_VALIDOS = ['XS','S','M','L','XL','XXL','XXXL','XXXXL','XXXXXL'];
+
 # Create your views here.
 def index(request):
     return render(request, "kits/index.html", {
         "kits": Kit.objects.all(),
         "titulo": "Kits"
+    })
+
+def add(request):
+    return render(request, "kits/add.html", {
+        "talles_validos": TALLES_VALIDOS,
+        "titulo": "Agregar nuevo kit"
     })
