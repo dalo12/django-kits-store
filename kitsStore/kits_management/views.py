b95a7872ea5d5bb36ebfbaa3bcb36f87c03781d9
David López <dalo_vidpez@hotmail.com.ar>
2024-03-13 18:11:39 -0300

    Created CRUD with framework

--- kitsStore/kits_management/views.py
+++ kitsStore/kits_management/views.py
@@ -2,7 +2,50 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Category, Kit, Team
 
+# Instanciar las vistas genéricas de Django
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from django.urls import reverse
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django import forms
+
 TALLES_VALIDOS = ['XS','S','M','L','XL','XXL','XXXL','XXXXL','XXXXXL'];
+
+class KitsList(ListView):
+    model = Kit
+
+class CreateKit(SuccessMessageMixin, CreateView):
+    model = Kit
+    form = Kit
+    fields = "__all__"
+    success_message = "Kit creado correctamente"
+
+    def get_success_url(self):
+        return reverse('index')
+
+class KitDetail(DetailView):
+    model = Kit
+
+class UpdateKit(SuccessMessageMixin, UpdateView):
+    model = Kit
+    form = Kit
+    fields = "__all__"
+    success_message = "Kit actualizado correctamente"
+
+    def get_success_url(self):
+        return reverse('index')
+
+class DeleteKit(SuccessMessageMixin, DeleteView):
+    model = Kit
+    form = Kit
+    fields = "__all__"
+
+    def get_success_url(self):
+        success_message = "Kit eliminado correctamente"
+        message.success (self.request, (success_message))
+        return reverse('index')
 
 # Create your views here.
 def index(request):
