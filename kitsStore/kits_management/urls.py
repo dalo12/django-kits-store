7fc3ab94de8ab8649baf704cec9637fb28b73e22
David López <dalo_vidpez@hotmail.com.ar>
2024-03-13 18:17:42 -0300

    Created a url to index

--- kitsStore/kits_management/urls.py
+++ kitsStore/kits_management/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.KitsList.as_view(template_name="index.html"), name="index"),
     path('add', views.add, name="add")
 ]
