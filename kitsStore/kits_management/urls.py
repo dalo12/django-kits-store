baf08151b6e2380e52a4fdf01d148ca51f7312d8
David López <dalo_vidpez@hotmail.com.ar>
2024-03-14 16:45:27 -0300

    Created 'delete' view

--- kitsStore/kits_management/urls.py
+++ kitsStore/kits_management/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.KitsList.as_view(template_name="kits/index.html"), name="index"),
     path('create', views.CreateKit.as_view(template_name="kits/create.html"), name="create"),
-    path('edit/<int:pk>', views.UpdateKit.as_view(template_name="kits/edit.html", name="edit"))
+    path('edit/<int:pk>', views.UpdateKit.as_view(template_name="kits/edit.html"), name="edit"),
+    path('delete/<int:pk>', views.DeleteKit.as_view(template_name="kits/delete.html"), name="delete")
 ]
