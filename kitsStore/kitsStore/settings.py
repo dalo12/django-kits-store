bda9fa77014559e982f1c8eac8c6bbaee1244c1a
David López <dalo_vidpez@hotmail.com.ar>
2024-03-14 19:08:42 -0300

    Fully installed django-bootstrap

--- kitsStore/kitsStore/settings.py
+++ kitsStore/kitsStore/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'crispy_forms',
+    'crispy_bootstrap5',
+    'django_bootstrap5',
     'kits_management',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -122,3 +125,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
